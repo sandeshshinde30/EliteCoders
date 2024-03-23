6598979c3dc5e352cf54ebc2ab5ecd9cdb2eecce
Sandesh Shinde <sandeshshinde7047@gmail.com>
2024-03-23 13:22:32 +0530

    Flutter project structure configured and some changes in main.dart with splash screen

---
+++ EliteCoders/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
+  // ThemeHelper().changeTheme('primary');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EduConsult',
+      debugShowCheckedModeBanner: false,
+      // initialRoute: AppRoutes.splashScreen,
+      // routes: AppRoutes.routes,
+    );
+  }
+}
+
---
+++ EliteCoders/lib/splash_screen/splash_screen.dart
@@ -0,0 +1,88 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+// import 'package:educonsult/core/app_export.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // timing();
+    checkLogin();
+  }
+
+
+  Future<void> checkLogin()
+  async
+  {
+    var loginOrNot;
+    var name;
+    var designation;
+    var prefCheckLogin = await SharedPreferences.getInstance();
+    loginOrNot = prefCheckLogin.getBool("login");
+    name = prefCheckLogin.getString("name");
+    designation = prefCheckLogin.getString("designation");
+
+    if(loginOrNot == null)
+    {
+      Timer(Duration(seconds:2),()=>Navigator.pushNamed(context,'/login_register_screen'));
+
+    }
+    // else
+    else if(!loginOrNot)
+    {
+      Timer(Duration(seconds:2),()=>Navigator.pushNamed(context,'/login_register_screen'));
+    }
+    else
+    {
+      print(name);
+      if(designation == "consultee") Timer(Duration(seconds:2),()=>Navigator.pushReplacementNamed(context,'/home_screen_consultee_screen'));
+      else Timer(Duration(seconds:2),()=>Navigator.pushReplacementNamed(context,'/home_screen_consultant_screen'));
+    }
+  }
+
+  // // After 2 seconds it will call intro screen
+  // Future<void> timing() async {
+  //   Timer(
+  //       Duration(seconds: 3),
+  //       () =>
+  //           Navigator.pushReplacementNamed(context, '/login_register_screen'));
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: double.maxFinite,
+          padding: EdgeInsets.symmetric(horizontal: 41.h),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomImageView(
+                imagePath: ImageConstant.imgEduconsultRemovebgPreview,
+                height: 200.v,
+                width: 220.h,
+              ),
+              SizedBox(height: 22.v),
+              Text(
+                "E D U C O N S U L T",
+                style: CustomTextStyles.titleMediumPrimary,
+              ),
+              SizedBox(height: 5.v),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
