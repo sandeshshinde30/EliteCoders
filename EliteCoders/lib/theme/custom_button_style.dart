38bb087cdd7eb315f9771cad623baaafb0e6ffc2
Md <mahemudborgave@gmail.com>
2024-03-24 02:17:37 +0530

    i am page button design

--- EliteCoders/lib/theme/custom_button_style.dart
+++ EliteCoders/lib/theme/custom_button_style.dart
@@ -87,7 +87,7 @@ class CustomButtonStyles {
   static ButtonStyle get fillPrimaryContainer => ElevatedButton.styleFrom(
     backgroundColor: theme.colorScheme.primaryContainer.withOpacity(1),
     shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(10.h),
+      borderRadius: BorderRadius.circular(26.h),
     ),
   );
 
